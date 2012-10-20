82ca5c705cfa568f3f05a4e54987816001941de6
Ben LeMasurier <ben.lemasurier@gmail.com>
2012-10-20 14:05:07 -0600

    explosion sound effects

--- explosion.c
+++ explosion.c
@@ -4,12 +4,15 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 
 #include "asteroids.h"
 #include "animation.h"
 #include "explosion.h"
 
 static ALLEGRO_BITMAP *sprites[15];
+static ALLEGRO_SAMPLE *sample;
 
 ANIMATION *
 explosion_create(VECTOR *position)
@@ -19,6 +22,8 @@ explosion_create(VECTOR *position)
   explosion->slowdown = 2;
   explosion->position->x = position->x - (explosion->width  / 2);
   explosion->position->y = position->y - (explosion->height / 2);
+
+  al_play_sample(sample, 1.0, 0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
 
   return explosion;
 }
@@ -31,6 +36,11 @@ explosion_init(void)
     sprintf(name, "data/sprites/asteroid/explosion/%d.png", i + 1);
     if((sprites[i] = al_load_bitmap(name)) == NULL)
       fprintf(stderr, "failed to load explosion sprite %d\n", i);
+  }
+  
+  if(!(sample = al_load_sample("data/sounds/explosion.wav"))) {
+    fprintf(stderr, "unable to load explosion audio sample.\n");
+    return false;
   }
 
   return true;
